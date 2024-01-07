73ed6de4754ffa0847a08f223df479e649545002
Willami <willami.bezerra@bobby.com.br>
2024-01-07 18:31:07 -0300

    add a observer

--- lib/app/modules/authentication/presentation/view/pages/register_account_page.dart
+++ lib/app/modules/authentication/presentation/view/pages/register_account_page.dart
@@ -11,47 +11,37 @@ class RegisterAccountPage extends StatefulWidget {
 }
 
 class _RegisterAccountPageState extends State<RegisterAccountPage> {
-  late TextEditingController _emailController;
-  late TextEditingController _passwordController;
-  @override
-  void initState() {
-    _emailController = TextEditingController();
-    _passwordController = TextEditingController();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: BodyRegisterAccount(
+      controller: widget.controller,
       size: size,
     ));
   }
 }
 
     // SizedBox(
-            //   width: double.maxFinite,
-            //   height: 50,
-            //   child: ElevatedButton(
-            //     onPressed: () {
-            //       widget.controller.createAccount(
-            //           email: _emailController.text,
-            //           password: _passwordController.text);
-            //     },
-            //     child: const Text('Entrar'),
-            //   ),
-            // ),
-            // Observer(
-            //   builder: (context) {
-            //     if (widget.controller.createAccountSucess != null) {
-            //       if (widget.controller.createAccountSucess!) {
-            //         return const Text('suceso');
-            //       } else {
-            //         return const Text('Falha');
-            //       }
-            //     } else {
-            //       return const Text('carregando');
-            //     }
-            //   },
-            // ),
+    //           width: double.maxFinite,
+    //           height: 50,
+    //           child: ElevatedButton(
+    //             onPressed: () {
+               
+    //             },
+    //             child: const Text('Entrar'),
+    //           ),
+    //         ),
+    //         Observer(
+    //           builder: (context) {
+    //             if (widget.controller.createAccountSucess != null) {
+    //               if (widget.controller.createAccountSucess!) {
+    //                 return const Text('suceso');
+    //               } else {
+    //                 return const Text('Falha');
+    //               }
+    //             } else {
+    //               return const Text('carregando');
+    //             }
+    //           },
+    //         ),
